6b8cd21069c299e24d690636df619d75b29c6814
Ivankov <ivan20481@gmail.com>
2018-03-31 13:05:48 +0300

    module1-task2 fix bag

--- js/main.js
+++ js/main.js
@@ -1,44 +1,45 @@
-(function () {
-  const KEY_ARROW = [37, 39];
-  const KEY_LEFT = 37;
-  const SCREEN = [`greeting`, `rules`, `game-1`, `game-2`, `game-3`, `stats`];
-  const template = document.querySelectorAll(`template`);
-  const mainCentral = document.querySelector(`main.central`);
-  let screenCurrent = 0;
+const KEY_ARROW = [37, 39];
+const KEY_LEFT = 37;
+const SCREEN = [`greeting`, `rules`, `game-1`, `game-2`, `game-3`, `stats`];
+const template = document.querySelectorAll(`template`);
+const mainCentral = document.querySelector(`main.central`);
+let screenCurrent = 0;
 
-  const changeScreen = (number) => {
-    if (number < 0) {
-      screenCurrent = 0;
-      return;
+const changeScreen = (number) => {
+  if (number < 0) {
+    screenCurrent = 0;
+    return;
+
+  }
+  if (number > SCREEN.length - 1) {
+    screenCurrent = SCREEN.length - 1;
+    return;
+  }
+
+  mainCentral.innerHTML = ``;
+  let nodeFind;
+  template.forEach(function (node) {
+    if (node.id === SCREEN[number]) {
+      nodeFind = node.cloneNode(true).content;
     }
-    if (number > SCREEN.length - 1) {
-      screenCurrent = SCREEN.length - 1;
-      return;
-    }
+  });
+  mainCentral.appendChild(nodeFind);
+};
 
-    while (mainCentral.firstChild) {
-      mainCentral.removeChild(mainCentral.firstChild);
-    }
-
-    mainCentral.appendChild(template[number].cloneNode(true).content);
-  };
-
-  const onKeyDown = (evt) => {
-    if (evt.altKey) {
-      const keyCode = evt.keyCode;
-      if (KEY_ARROW.indexOf(evt.keyCode) > -1) {
-        evt.preventDefault();
-        if (keyCode === KEY_LEFT) {
-          screenCurrent--;
-        } else {
-          screenCurrent++;
-        }
-        changeScreen(screenCurrent);
+const onKeyDown = (evt) => {
+  if (evt.altKey) {
+    const keyCode = evt.keyCode;
+    if (KEY_ARROW.indexOf(keyCode) > -1) {
+      evt.preventDefault();
+      if (keyCode === KEY_LEFT) {
+        screenCurrent--;
+      } else {
+        screenCurrent++;
       }
+      changeScreen(screenCurrent);
     }
-  };
+  }
+};
 
-  document.addEventListener(`keydown`, onKeyDown);
-  changeScreen(screenCurrent);
-
-})();
+document.addEventListener(`keydown`, onKeyDown);
+changeScreen(screenCurrent);
